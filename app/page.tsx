14c72b10875592367d5fc126569a0e10f97392f0
davepapichulo <davidasukelly@gmail.com>
2024-07-31 23:20:39 +0100

    Designed Interface

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/outline'
 
 
 export default function Home() {
@@ -5,19 +6,58 @@ export default function Home() {
     <div className="text-white flex flex-col items-center justify-center h-screen px-2">
       <h1 className=" text-5xl font-bold mb-20">ChatGPT</h1>
 
-      <div>
+      <div className=' flex space-x-2 text-center '>
+
         <div>
-          <div>
-            {/* Sun icon */}
-            <h2 className=" flex flex-col items-center justify-center mb-5">Examples</h2>
+
+          <div className=" flex flex-col items-center justify-center mb-5" >
+            <SunIcon className="size-8" />
+            <h2> Examples </h2>
           </div>
+
           <div className=" space-y-2">
-            <p className=" infoText">Explain Something to me</p>
-            <p className=" infoText">The Difference between a Cat and a Dog</p>
+            <p className=" infoText">"Explain quantum computing in simple terms"</p>
+            <p className=" infoText">What is the difference between a Cat and a Dog</p>
             <p className=" infoText">What is the color of the sun</p>
           </div>
+
         </div>
+        <div>
+
+          <div className=" flex flex-col items-center justify-center mb-5" >
+            <BoltIcon className="size-8" />
+            <h2> Capabilities </h2>
+          </div>
+
+          <div className=" space-y-2">
+            <p className=" infoText">Remembers what user said earlier in the conversation</p>
+            <p className=" infoText">Allows user to provide follow-up corrections</p>
+            <p className=" infoText">Trained to decline inappropriate requests</p>
+          </div>
+
+          </div>
+
+          <div>
+
+          <div className=" flex flex-col items-center justify-center mb-5" >
+            <ExclamationTriangleIcon className="size-8" />
+            <h2> Limitations</h2>
+          </div>
+
+          <div className=" space-y-2">
+            <p className=" infoText">May occasionally generate incorrect information</p>
+            <p className=" infoText">May occasionally produce harmful instructions or biased
+            content</p>
+            <p className=" infoText">Limited knowledge of world and events after 2021</p>
+          </div>
+
+          </div>
+
+
+
       </div>
+
+
     </div>
   );
 }
